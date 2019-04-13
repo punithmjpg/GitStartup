89c42494cce5c29127e005d5fb262390c2d1d221
Ubuntu <ubuntu@ip-172-31-15-10.ap-south-1.compute.internal>
2019-04-13 03:53:40 +0000

    script

---
+++ for.sh
@@ -0,0 +1,6 @@
+for (( counter=0; counter<30; counter+=5 ))
+do
+echo -n "$counter "
+done
+printf "\n"
+
---
+++ while.sh
@@ -0,0 +1,19 @@
+
+t=1
+while [ $valid ]
+do
+echo $count
+if [ $count -eq 5 ];
+then
+break
+fi
+valid=true
+count=1
+while [ $valid ]
+do
+echo $count
+if [ $count -eq 5 ];
+then
+break
+fi
+
